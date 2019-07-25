837bb9919563d3cd627d98e33bd86771462065d3
SHAIK <jaffar.shaik@capgemini.com>
2019-07-25 15:41:52 +0530

    first commit

---
+++ PromoServiceCosmos/PromoServiceCosmos/DataAccess/ICosmosDataAdapter.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+using PromoServiceCosmos.Models;
+
+namespace PromoServiceCosmos.DataAccess
+{
+    public interface ICosmosDataAdapter
+    {
+        Task<bool> CreateDatabase(string name);
+        Task<bool> CreateCollection(string dbName, string name);
+        Task<bool> CreateDocument(string dbName, string name, ProductPromo productpromo);
+        Task<ProductPromo> DeleteUserAsync(string dbName, string name, string id);
+
+        Task<dynamic> GetData(string dbName, string name);
+        //Task<dynamic> GetData(string dbName, string name, ProductPromo productpromo);
+        Task<bool> updateDocumentAsync(string dbName, string name, ProductPromo productpromo);
+        Task<dynamic> GetDataById(string dbName, string name, string id);
+    }
+}
